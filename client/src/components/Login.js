837e5002456e07e3b7dd346f77368bdea32d3902
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-05-15 16:43:50 -0700

    created login and axWithAuth

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  console.log("login: ", username, password);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    axios
+      .post("http://localhost:5000/api/login", { username, password })
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/bubble-page");
+        setUsername("");
+        setPassword("");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    setIsLoading(false);
+  };
+
   return (
-    <>
+    <div className="login">
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <h2>{isLoading ? "...loading" : "Login"}</h2>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
   );
 };
 
---
+++ client/src/components/axiosWithAuth.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const axiosWithAuth = () => {
+  return axios.create({
+    headers: { authorization: localStorage.getItem("token") },
+  });
+};
+
+export default axiosWithAuth;
